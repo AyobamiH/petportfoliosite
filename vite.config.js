363f1933575259cc047f5957da0501abce220fb1
AyobamiH <heryourbarme@live.com>
2023-12-12 13:47:46 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig ({
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // For example, split React and its related libraries into their own chunk
-            if (id.includes('react')) {
-              return 'react-vendor';
-            }
-            // Split GSAP into its own chunk
-            if (id.includes('gsap')) {
-              return 'gsap-vendor';
-            }
-            // Split other large dependencies
-            return 'vendor';
-          }
+        manualChunks: {
+          'vendor': ['react', 'react-dom']
         }
       }
-    }
+    },
+    chunkSizeWarningLimit: 600 // Adjust chunk size limit
   }
 });
+
+
