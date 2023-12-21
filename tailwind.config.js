741c3194ef7d2beed0674b54e48596c254e0508b
AyobamiH <heryourbarme@live.com>
2023-12-21 15:43:59 +0000

    added src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ export default {
         'gradient-linear': 'linear-gradient(90deg, #353763, #b8aa84, #716170, #5b6669)',
         'hero-pattern': "url('/images/background.jpg')",
       },
+      boxShadow: {
+        'left': '0px 0 0px -5px rgba(0, 0, 0, 0.5)', // Custom shadow for the left side
+      },
+      screens: {
+        'xs': '320px',   // Custom breakpoint for very small devices
+        'sm': '640px',   // Default small breakpoint (mobile)
+        'md': '768px',   // Default medium breakpoint (tablet)
+        'lg': '1024px',  // Default large breakpoint (laptop)
+        'xl': '1280px',  // Extra large breakpoint (desktop)
+        '2xl': '1536px', // 2X large breakpoint for very large screens
+      },
       keyframes: {
         'gradient-move': {
           '0%, 100%': { backgroundPosition: '0% 50%' },
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig ({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // For example, split React and its related libraries into their own chunk
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            // Split GSAP into its own chunk
+            if (id.includes('gsap')) {
+              return 'gsap-vendor';
+            }
+            // Split other large dependencies
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
