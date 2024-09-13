f67ec960456c0c163d92abf8aea03878289bf7ba
AyobamiH <heryourbarme@live.com>
2024-09-13 14:14:09 +0100

    Navigation

--- src/components/PetSitters/Navigation.jsx
+++ src/components/PetSitters/Navigation.jsx
@@ -1,8 +1,8 @@
-
 import React, { useState } from "react";
 import Logo from "./animations/logoDark.json";
 import Lottie from "lottie-react";
 import { Link } from "react-router-dom";
+
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,42 +12,38 @@ const Navigation = () => {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              
-
-              <div className="">
-        
-        <Link to='/'>
-          <Lottie className="w-16 h-9 object-cover opacity-30"
-          animationData={Logo} loop={true} />
-        </Link>
-      </div>
+              <div>
+                <Link to="/">
+                  <Lottie className="w-16 h-9 object-cover opacity-30" animationData={Logo} loop={true} />
+                </Link>
+              </div>
             </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-            <a
-              href="#"
-              className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            <Link
+              to="/"
+              className="hover:border-[#FF804B] text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             >
               Home
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            </Link>
+            <Link
+              to="/services"
+              className="border-transparent hover:border-[#FF804B] text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             >
               Services
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            </Link>
+            <Link
+              to="/portfolio"
+              className="border-transparent hover:border-[#FF804B] text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             >
               Portfolio
-            </a>
-            <a
-              href="#"
-              className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+            </Link>
+            <Link
+              to="/contact"
+              className="border-transparent text-gray-500 hover:border-[#FF804B] hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             >
               Contact
-            </a>
+            </Link>
           </div>
           <div className="-mr-2 flex items-center sm:hidden">
             <button
@@ -97,30 +93,30 @@ const Navigation = () => {
       {isOpen && (
         <div className="sm:hidden">
           <div className="pt-2 pb-3 space-y-1">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
               Home
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/services"
               className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
               Services
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/portfolio"
               className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
               Portfolio
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/contact"
               className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
               Contact
-            </a>
+            </Link>
           </div>
         </div>
       )}
