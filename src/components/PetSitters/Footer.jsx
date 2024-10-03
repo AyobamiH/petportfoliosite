ea838496353462c3f1e6b2cb68121a991ab71bc8
AyobamiH <heryourbarme@live.com>
2024-10-03 14:25:51 +0100

    Footer

--- src/components/PetSitters/Footer.jsx
+++ src/components/PetSitters/Footer.jsx
@@ -7,6 +7,15 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div>
+            <h3 className="text-xl font-bold mb-4">Tail-Wagging Websites</h3>
+            <p>Custom web design tailored for pet care services.</p>
+            <div className="flex mt-4 space-x-4">
+              {/* <a href="#" className="text-white">FB</a>
+              <a href="#" className="text-white">TW</a>
+              <a href="#" className="text-white">IG</a> */}
+            </div>
+          </div>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
             <h3 className="text-lg font-semibold mb-4">Quick Links</h3>
